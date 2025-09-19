0d1bd2fbbb74cc7ff12b8d2876dc6d5e29e85810
Cristian Tagle <cristian.gonzalez.gt@gmail.com>
2025-09-19 14:38:47 -0300

    fix: 12 ítems por categoría + imagen Banda Elástica + images config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
-  images: { formats: ['image/avif', 'image/webp'] },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },],
+    unoptimized: true, formats: ['image/avif', 'image/webp'] },
 }
 module.exports = nextConfig
